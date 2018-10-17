65db9a64b13105275651e2c3ed599c706ed8100b
Angeline <atan@thoughtworks.com>
2018-10-17 17:49:06 +0800

    Modify branch one to remove unrequired dependencies

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const _ = require('lodash');
 const PORT = 3000
 
-app.use(express.static('client'));
 app.use(express.json())
 
 let songs = [];
-let id = 0;
 
 //return list of all songs
 
