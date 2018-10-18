3c8c1cc1aca5152be25677e46ac21fd8d998714d
Angeline <atan@thoughtworks.com>
2018-10-18 14:32:47 +0800

    Update test in branch 4 to be more concise. Remove unused dependancy from package.json.

--- tests/songs.test.js
+++ tests/songs.test.js
@@ -4,36 +4,32 @@ const request = require("supertest");
 describe("routes/songs", () => {
 
   it("POST /songs should return a new song object", () => {
-    requestBody = { name: "test song", artist: "rhianna"};
+    requestBody = {name: "test song", artist: "rhianna"};
     responseBody = {id: 1, name: "test song", artist: "rhianna"};
     return request(app)
     .post("/songs")
     .send(requestBody)
     
     .then(response => {
-
       expect(response.status).toEqual(201);
       expect(response.body).toEqual(responseBody)
     });
   });
   
-  it("GET /songs should return a non empty array", () => {
+  it("GET /songs should return an array containing one song", () => {
     return request(app)
     .get("/songs")
     
     .then(response => {
       expect(response.status).toEqual(200);
       expect(Array.isArray(response.body)).toEqual(true);
-      expect(response.body.length).toBeGreaterThan(0);
+      expect(response.body.length).toEqual(1);
     });
   });
   
   it("PUT /songs should return the updated song", () => {
-    requestBody = {
-      id: 1,
-      name: "updated song",
-      artist: "rhianna"
-    };
+    requestBody = {name: "updated song", artist: "rhianna"};
+    responseBody = {id: 1, name: "updated song", artist: "rhianna"};
     
     return request(app)
     .put("/songs/1")
@@ -41,17 +37,13 @@ describe("routes/songs", () => {
     
     .then(response => {
       expect(response.status).toEqual(200);
-      expect(response.body).toEqual(requestBody);
+      expect(response.body).toEqual(responseBody);
     });
   });
 
   it("DELETE /songs/:id should return the deleted song", () => {
     const ID = 1;
-    expected = {
-      id: 1,
-      name: "updated song",
-      artist: "rhianna"
-    };
+    expected = { id: 1, name: "updated song", artist: "rhianna" };
 
     return request(app)
     .delete("/songs/1")
