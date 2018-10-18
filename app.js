ee945d64a47a2029113b1299e1d37d4307b2bdf9
Angeline <atan@thoughtworks.com>
2018-10-18 15:42:56 +0800

    Update state of branch 5 to match previous lab 4-2
    - include app.params in this lab
    - update movies test to be concise 
    - remove unused dependancies

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('client'));
 app.use(express.json())
 
 let songs = [];
 let movies = [];
 
+
 //Songs API
+
+app.param('id', (req, res, next, id) => {
+  let song = songs.find(song => song.id === parseInt(id));
+  if (song) {
+    req.song = song;
+    next();
+  } 
+  else 
+  {
+    res.status(404).json({message : "Resource not found"});
+  }
+});
+
 //return list of all songs
 app.get('/songs', (req, res) => {
   res.status(200).json(songs);
@@ -26,26 +39,23 @@ app.post('/songs', (req, res) => {
 
 //return a song with id 
 app.get('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id == parseInt(req.params.id));
-  res.status(200).json(song);
+  res.status(200).json(req.song);
 });
 
 //edit a song with id, and return edited song
 app.put('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id === parseInt(req.params.id));
-  song.name = req.body.name;
-  song.artist = req.body.artist;
-  res.status(200).json(song);
+  req.song.name = req.body.name;
+  req.song.artist = req.body.artist;
+  res.status(200).json(req.song);
 });
 
 //delete a song with id, and return deleted song
 app.delete("/songs/:id", (req, res) => {
-  let songToDelete = songs.find(song => song.id === parseInt(req.params.id));
+  let songToDelete = req.song
   let index = songs.indexOf(songToDelete);
   songs.splice(index, 1);
   res.status(200).json(songToDelete);
 });
-
 
 // Movies API
 //return list of all movies
@@ -65,22 +75,23 @@ app.post('/movies', (req, res) => {
 });
 
 //return a movie with id 
-app.get('/movies/:id', (req, res) => {
-  let movie = movies.find(movie => movie.id == parseInt(req.params.id));
+app.get('/movies/:movieId', (req, res) => {
+  let movie = movies.find(movie => movie.id == parseInt(req.params.movieId));
   res.status(200).json(movie);
 });
 
 //edit a movie with id, and return edited movie
-app.put('/movies/:id', (req, res) => {
-  let movie = movies.find(movie => movie.id === parseInt(req.params.id));
+app.put('/movies/:movieId', (req, res) => {
+  console.log(req.params.id)
+  let movie = movies.find(movie => movie.id === parseInt(req.params.movieId));
   movie.name = req.body.name;
   movie.artist = req.body.artist;
   res.status(200).json(movie);
 });
 
 //delete a movie with id, and return deleted movie
-app.delete("/movies/:id", (req, res) => {
-  let movieToDelete = movies.find(movie => movie.id === parseInt(req.params.id));
+app.delete("/movies/:movieId", (req, res) => {
+  let movieToDelete = movies.find(movie => movie.id === parseInt(req.params.movieId));
   let index = movies.indexOf(movieToDelete);
   movies.splice(index, 1);
   res.status(200).json(movieToDelete);
