0a9eac28f02902df75cfc5f8cb7d29d60ac6f7c0
n4t0b0t <natalieong1990@users.noreply.github.com>
2019-06-25 10:57:24 +0800

    latest version for heroku

--- routes/songs.js
+++ routes/songs.js
@@ -1,151 +1,145 @@
-const Joi = require('joi');
-const express = require('express');
+const Joi = require("joi");
+const express = require("express");
 const router = express.Router();
 
 let songs = [];
-const DELAY = 10
+const DELAY = 10;
 
 //Integrate below methods in the route handlers with async and await
 const getSongs = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(songs);
-          }, DELAY);
-    });
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(songs);
+    }, DELAY);
+  });
+};
 
-const createSong = (requestBody) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let newSong = {
-                id: songs.length + 1,
-                name: requestBody.name,
-                artist: requestBody.artist 
-            }
+const createSong = requestBody => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let newSong = {
+        id: songs.length + 1,
+        name: requestBody.name,
+        artist: requestBody.artist
+      };
 
-            songs.push(newSong);
-            resolve(newSong)
-        }, DELAY);
-    });
-}
+      songs.push(newSong);
+      resolve(newSong);
+    }, DELAY);
+  });
+};
 
-const getSong = (id) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let songFound = songs.find(song => song.id == parseInt(id));
-            if (!songFound){
-                reject(new Error(`Unable to find song with id: ${id}`))
-            }
-            return resolve(songFound);
-        }, DELAY);
-    })
-}
+const getSong = id => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let songFound = songs.find(song => song.id == parseInt(id));
+      if (!songFound) {
+        reject(new Error(`Unable to find song with id: ${id}`));
+      }
+      return resolve(songFound);
+    }, DELAY);
+  });
+};
 
 const updateSong = (requestBody, songToUpdate) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            songToUpdate.name = requestBody.name;
-            songToUpdate.artist = requestBody.artist;
-            resolve(songToUpdate)
-        }, DELAY);
-    });
-}
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      songToUpdate.name = requestBody.name;
+      songToUpdate.artist = requestBody.artist;
+      resolve(songToUpdate);
+    }, DELAY);
+  });
+};
 
-const deleteSong = (songToDelete) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if(!songToDelete) {
-                reject(new Error())
-            }
+const deleteSong = songToDelete => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!songToDelete) {
+        reject(new Error());
+      }
 
-            let index = songs.indexOf(songToDelete);
-            songs.splice(index, 1);
-            resolve(songToDelete);
-        }, DELAY)
-    })
-}
+      let index = songs.indexOf(songToDelete);
+      songs.splice(index, 1);
+      resolve(songToDelete);
+    }, DELAY);
+  });
+};
 
 //Song API
-router.param('id', (req, res, next, id) => {
-    let song = songs.find(song => song.id === parseInt(id));
-    if(!song){
-        const error = new Error(`Unable to find song with id: ${id}`);
-        error.statusCode = 404;
-        return next(error)
-    }
-    req.song = song;
-    next();
+router.param("id", async (req, res, next, id) => {
+  req.song = await getSong(id).catch(e => {
+    e.statusCode = 404;
+    next(e);
+  });
+  next();
 });
 
 //return list of all songs
-router.get('/', (req, res, next) => {
-    res.status(200).json(songs);
+router.get("/", async (req, res, next) => {
+  const output = await getSongs();
+  res.status(200).json(output);
 });
 
 //create a new song, and return new song
-router.post('/', (req, res, next) => {
-    const validation = validateSong(req.body)
-    if(validation.error) {
-        let error = new Error(validation.error.details[0].message)
-        error.statusCode = 400
-        return next(error); 
-    } 
-    
-    let newSong = {
-        id: songs.length + 1,
-        name: req.body.name,
-        artist: req.body.artist 
-    }
-    songs.push(newSong);
-    res.status(201).json(newSong);
+router.post("/", async (req, res, next) => {
+  const validation = validateSong(req.body);
+  if (validation.error) {
+    let error = new Error(validation.error.details[0].message);
+    error.statusCode = 400;
+    return next(error);
+  }
+
+  const output = await createSong(req.body);
+  res.status(201).json(output);
 });
 
-//return a song with id 
-router.get('/:id', (req, res, next) => {
-    res.status(200).json(req.song);
+//return a song with id
+router.get("/:id", async (req, res, next) => {
+  res.status(200).json(req.song);
 });
 
 //update a song with id, and return edited song
-router.put('/:id', (req, res, next) => {
-    const validation = validateSong(req.body)
-    if (validation.error){
-        let error = new Error(validation.error.details[0].message)
-        error.statusCode = 400
-        return next(error);
-    }
+router.put("/:id", async (req, res, next) => {
+  const validation = validateSong(req.body);
+  if (validation.error) {
+    let error = new Error(validation.error.details[0].message);
+    error.statusCode = 400;
+    return next(error);
+  }
 
-    req.song.name = req.body.name;
-    req.song.artist = req.body.artist;
-    res.status(200).json(req.song);
+  const output = await updateSong(req.body, req.song);
+  res.status(200).json(output);
 });
 
 //delete a song with id, and return deleted song
-router.delete("/:id", (req, res, next) => {
-
-    let index = songs.indexOf(req.song);
-    songs.splice(index, 1);
-    res.status(200).json(req.song);
+router.delete("/:id", async (req, res, next) => {
+  const output = await deleteSong(req.song).catch(e => next(e));
+  res.status(200).json(output);
 });
 
 //Add error handler for songs router to return error on failure at any route
 router.use(function(err, req, res, next) {
-    // If err has no error code, set error code to 500
-    if (!err.statusCode){
-        err.statusCode = 500; 
-        err.message = { message: "Internal server error"}
-    }
+  // If err has no error code, set error code to 500
+  if (!err.statusCode) {
+    err.statusCode = 500;
+    err.message = { message: "Internal server error" };
+  }
 
-    // send back specified status code and message
-    res.status(err.statusCode).json({ message : err.message}); 
+  // send back specified status code and message
+  res.status(err.statusCode).json({ message: err.message });
 });
 
-function validateSong(song){
-    const schema = {
-        id: Joi.number().integer(),
-        name: Joi.string().min(3).required(),
-        artist: Joi.string().min(3).required()
-    }
-    return Joi.validate(song, schema);
+function validateSong(song) {
+  const schema = {
+    id: Joi.number().integer(),
+    name: Joi.string()
+      .min(3)
+      .required(),
+    artist: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(song, schema);
 }
 
 module.exports = router;
